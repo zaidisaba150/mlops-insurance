47b17c4db4403a7634b1867e2502b0794dc88b9d
zaidisaba150 <zaidi.saba150@gmail.com>
2024-12-19 00:09:59 +0530

    version 2

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="saba zaidi",
+    author_email="zaidi.saba150@gmail.com",
+    packages=find_packages()
+)
